dda90a5d5a0c5bc47bc4c8ecf03511e885cc9193
Apprentice <nguyenhstephen@gmail.com>
2013-05-17 18:03:08 -0700

    can select next card after guess

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,7 +2,18 @@ get '/game' do
   erb :new_game
 end
 
-get '/game/:deck_id' do
+
+post '/game/:deck_id' do
   @deck = Deck.find(params[:deck_id])
+  @next_card = Card.find(params[:next_card_id])
+
+  redirect "/game/#{@deck.id}/#{@next_card.id}"
+end
+
+get '/game/:deck_id/:card_id' do
+  @deck = Deck.find(params[:deck_id])
+  @card = Card.find(params[:card_id])
+
+  
   erb :play
 end
