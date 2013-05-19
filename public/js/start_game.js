d320931ec64f0c4aa1e5c5516d34e8ac270f05b8
dbc-warrior <apprentice@devbootcamp.com>
2013-05-18 17:06:10 -0700

    started ajaxing game controller'

--- public/js/start_game.js
+++ public/js/start_game.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
- $('.decks').click("explode");
+  $('#card_guess, form').on('submit', function(e){
+    e.preventDefault();
+    $.ajax({
+      type: 'post',
+      url: $('#card_guess').attr('action'),
+      data: $('#card_guess').serialize()
+    }).done(function(data){
+      console.log(data);
+    });
+  });
 
 });
+
