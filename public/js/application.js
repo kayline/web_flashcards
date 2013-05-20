35ee9b1d155bcdf8a71fb06e55e5d38dffdce80c
Brian Pope <pope1280@gmail.com>
2013-05-19 20:56:33 -0700

    slowed down js intro

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-  $(".home").find("h1").text("Having Trouble with Memorization?!").fadeIn(6000).fadeOut(4000, function() {
-    $(this).text("We're here to help with that!").fadeIn(2000).fadeOut(2000, function () {
-      $(this).text("Welcome to FlashyFlash!").fadeIn(2000, function () {
-        $(".home").find("p").slideUp(1000).text("Please Login or Sign Up to get started").slideDown(1500);
+  $(".home").find("h1").text("Having Trouble with Memorization?!").fadeIn(6000).fadeOut(2000, function() {
+    $(this).text("We're here to help with that!").fadeIn(1500).fadeOut(1000, function () {
+      $(this).text("Welcome to FlashyFlash!").fadeIn(1500, function () {
+        $(".home").find("p").slideUp(500).text("Please Login or Sign Up to get started").slideDown(1500);
 
       });
     });
